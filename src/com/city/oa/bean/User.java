775702493d44f668aa4bd46916b34a68f39cb8fa
张三 <zhangsan@gmail>
2019-09-11 00:41:04 +0800

    提交到远程版本库

---
+++ src/com/city/oa/bean/User.java
@@ -0,0 +1,29 @@
+package com.city.oa.bean;
+
+public class User {
+
+    private String username;
+    private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
